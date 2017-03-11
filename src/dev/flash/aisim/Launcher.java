52af5d3a410d2b3a5625dd321d3456a990463ecd
Flash <Flash.Jaq@gmail.com>
2017-03-11 19:55:32 +0000

    Initial Commit

---
+++ src/dev/flash/aisim/Launcher.java
@@ -0,0 +1,12 @@
+package dev.flash.aisim;
+
+/**
+ * Created by Flash on 11/03/2017.
+ */
+
+public class Launcher {
+	public static void main(String[] args) {
+		Instance instance = new Instance("AI SIM", 1100, 600);
+		instance.start();
+	}
+}
