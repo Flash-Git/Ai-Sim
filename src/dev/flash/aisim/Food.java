e4d82e8a434be03e3ac8423b82f25d1f286c9b17
Flash <flash.jaq@gmail.com>
2017-03-28 12:39:08 +0100

    Added food class

---
+++ src/dev/flash/aisim/Food.java
@@ -0,0 +1,22 @@
+package dev.flash.aisim;
+
+/**
+ * Created by Flash on 28/03/2017.
+ */
+
+public class Food {
+
+private Vector2 pos;
+	
+	public Food(Vector2 pos) {
+		this.pos = pos;
+	}
+	
+	public Vector2 getPos() {
+		return pos;
+	}
+	
+	public void setPos(Vector2 pos) {
+		this.pos = pos;
+	}
+}
